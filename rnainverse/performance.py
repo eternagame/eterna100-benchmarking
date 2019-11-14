b8287a93db6f9c0e9c9e3b329ae2e51f0857ffee
Rohan Koodli <rovik05@gmail.com>
2019-11-14 11:30:31 -0800

    Track progress & assert length equality

--- rnainverse/performance.py
+++ rnainverse/performance.py
@@ -9,14 +9,19 @@ if __name__ == '__main__':
 	
 	solved = []
 	names = []
-
+        
+        i = 1
 	for puzzle in puzzle_file:
+                assert(len(puzzle[2]) == len(puzzle[1]), 'Starting sequence length and puzzle length do not match')
 		result = RNA.inverse_fold(puzzle[2], puzzle[1])[1]
 		names.append(puzzle[0])
 		if result == 0.0:
 			solved.append(1)
 		else:
 			solved.append(0)
+                print('Completed %i/100: %s' % (i, puzzle[0]))
+
+                i += 1
 
 	for res, name in zip(solved, names):
 		print('%s: %i' % (name, res))
