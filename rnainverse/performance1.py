6761564b699ac147c5a06d649b541ea742c6d65e
Rohan Koodli <rovik05@gmail.com>
2020-08-08 20:37:56 -0700

    Clean up run methods

--- rnainverse/performance1.py
+++ rnainverse/performance1.py
@@ -22,8 +22,7 @@ def fold(struc):
 	formatted = re.split('\s+| \(?\s?',pair)
 	return formatted[1]
 
-
-if __name__ == '__main__':
+def run_old():
 	df = pd.read_csv(os.getcwd() + '/eterna100_v1_tabs.txt', sep='\t', delimiter='\t', names=['Name', 'Structure', 'Start', 'Locks'])
 	struc = df['Structure'].tolist()
 	start = df['Start'].tolist()
@@ -57,4 +56,35 @@ if __name__ == '__main__':
 		for res in solved:
 			f.write("%i\n" % res)
 	print('\nSolved %i/100' % sum(solved))
+
+def run():
+	df = pd.read_csv(os.getcwd() + '/eterna100_v1_tabs.txt', sep='\t', delimiter='\t', names=['Name', 'Structure', 'Start', 'Locks'])
+
+	solved = []
+	names = []
+	tot = 0
+	for i in tqdm(range(100)):
+		puzzle = df.iloc[i]
+		name = puzzle['Name']
+		struc = puzzle['Structure']
+		start = puzzle['Start'].upper()
+
+		solution = inverse_fold(struc, start)
+
+		with open(os.getcwd()+'/rnai_puzzle_solutions_v1.txt', 'a') as f:
+			f.write('%i\t%s\t%s\t%s\n' % (i, name, struc, solution))
+		
+
+		with open(os.getcwd()+'/rnai_puzzle_results_v1.txt', 'a') as f:
+			if struc == fold(solution):
+				f.write('%i\n' % 1)
+				tot += 1
+			else:
+				f.write('%i\n' % 0)
+			
+
+	print('\nSolved %i/100' % tot)
+
+if __name__ == '__main__':
+	run()
 	
