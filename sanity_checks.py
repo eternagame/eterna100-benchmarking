56a1926ada3a2739f0897dd1e1fcf63c2f237339
Rohan Koodli <rovik05@gmail.com>
2023-12-09 21:59:58 -0800

    popen

--- sanity_checks.py
+++ sanity_checks.py
@@ -1,8 +1,25 @@
 import RNA
 import pandas as pd
 import os
+from typing import Literal
+from subprocess import Popen, PIPE, STDOUT
 
-def check_v2_sequences(outfile='v2_sanity_check.txt'):
+VIENNA_VERSIONS = Literal['latest', '2.1.9', '2.4.8']
+
+def fold(seq, version: VIENNA_VERSIONS = 'latest'):
+    if version == 'latest':
+        return RNA.fold(seq)[0]
+    else:
+        if version == '2.1.9'
+            p = Popen(['.././ViennaRNA219/bin/RNAfold', '-T','37.0'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, encoding='utf8')
+        elif version == '2.4.8':
+            p = Popen(['.././ViennaRNA248/bin/RNAfold', '-T','37.0'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, encoding='utf8')
+        pair = p.communicate(input=''.join(seq))[0]
+        formatted = re.split('\s+| \(?\s?',pair)
+        return formatted[1]
+
+
+def check_v2_sequences(outfile: str = 'v2_sanity_check.txt', version: VIENNA_VERSIONS = 'latest'):
     e100 = pd.read_csv('eterna100_vienna2.txt', sep='\t', header='infer')
     # make the 'Sample Solution (1)' and 'Sample Solution (2)' columns lists
     sols1 = e100['Sample Solution (1)'].tolist()
@@ -13,9 +30,9 @@ def check_v2_sequences(outfile='v2_sanity_check.txt'):
     buggy = []
     f = open(outfile, 'w')
     for i in range(100):
-        struc1 = RNA.fold(sols1[i])[0]
+        struc1 = fold(sols1[i], version)
         if struc1 != strucs[i]:
-            struc2 = RNA.fold(sols2[i])[0]
+            struc2 = fold(sols2[i], version)
             if struc2 != strucs[i]:
                 f.write(f'{names[i]}\t{strucs[i]}\t{sols1[i]}\t{struc1}\t{sols2[i]}\t{struc2}\n')
                 buggy.append(names[i])
