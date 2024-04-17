8191d18f97b5c739bd3611be53cf3803f9e10c33
Jonathan Romano <jonathan@luxaritas.com>
2024-04-17 10:02:52 -0700

    Slurm support for queue_train

---
+++ scripts/util/slurm.py
@@ -0,0 +1,62 @@
+import os
+import re
+from typing import Union
+from subprocess import run
+
+root_path = os.path.join(os.path.dirname(__file__), '../../')
+
+def sbatch(
+    commands: Union[str, list[str]],
+    job_name,
+    timeout: str = None,
+    partition: str = None,
+    cpus: int = None,
+    gpus: int = None,
+    memory: str = None,
+    memory_per_cpu: str = None,
+    ntasks: int = None,
+    dependency: str = None,
+    mail_type: str = None,
+    constraint: str = None,
+):
+    args = ['sbatch']
+
+    if job_name is not None:
+        args.append(f'--job-name="{job_name}"')
+        args.append(f'--output="data/slurm/%j-{job_name}"')
+    
+    if timeout is not None:
+        args.append(f'--time={timeout}')
+
+    if partition is not None:
+        args.append(f'--partition={partition}')
+
+    if cpus is not None:
+        args.append(f'--cpus-per-task={cpus}')
+    
+    if gpus is not None:
+        args.append(f'--cpus-per-task={gpus}')
+
+    if memory is not None:
+        args.append(f'--mem={memory}')
+    
+    if memory_per_cpu is not None:
+        args.append(f'--mem-per-cpu={memory_per_cpu}')
+
+    if ntasks is not None:
+        args.append(f'--ntasks={ntasks}')
+
+    if dependency is not None:
+        args.append(f'--dependency={dependency}')
+    
+    if mail_type is not None:
+        args.append(f'--mail-type={mail_type}')
+    
+    if constraint is not None:
+        args.append(f'-C "{constraint}"')
+
+    input = '#!/bin/sh' + (input if isinstance(commands, str) else '\n'.join(commands))
+
+    res = run(args, input=input, cwd=root_path, capture_output=True)
+    # Return job ID of queued job
+    return re.search(r'\d+', res.stdout.decode())[0]
