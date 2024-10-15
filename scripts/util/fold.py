20cb3784bbdab9a3a148ae09b9c55b8fdac16a6b
Jonathan Romano <jonathan@luxaritas.com>
2024-10-15 10:07:58 -0400

    Further improved logging of rnafold errors

--- scripts/util/fold.py
+++ scripts/util/fold.py
@@ -12,5 +12,5 @@ def fold(seq, version: VIENNA_VERSIONS):
     out = p.communicate(input=seq)[0]
     match = re.search(r'^(?:WARNING:.+\n)*(.+)\n(.+)\s+\(\s*(.+)\)(?:\n(?:.|\n)+)?\n?$', out)
     if not match:
-        raise RuntimeError('Could not parse output of RNAfold:', out)
+        raise RuntimeError('Could not parse output of RNAfold for seq', seq, ':', out)
     return match.group(2), match.group(3)
